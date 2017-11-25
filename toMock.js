979beb56d8044d9ddb298ad58a37d6348c7ca77f
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2017-11-25 10:21:41 +0100

    #! updated code style

--- toMock.js
+++ toMock.js
@@ -1,112 +1,120 @@
-Object.defineProperty(exports, "__esModule", {
-	value: true
+Object.defineProperty(exports, '__esModule', {
+  value: true
 });
 
-const PROTOTYPE_CHAIN_MOCKED = Symbol('prototypeChainMocked');
+const MOCKED_PROTOTYPE_CHAIN = Symbol('mockedPrototypeChain');
 
 function toMock(arg) {
-	if (typeof arg === 'function') {
-		return mockClass(arg);
-	}
+  if (typeof arg === 'function') {
+    return mockClass(arg);
+  }
 
-	if (typeof arg === 'object') {
-		return mockObject(arg);
-	}
+  if (typeof arg === 'object') {
+    return mockObject(arg);
+  }
 
-	throw new TypeError(`The toMock method support object and ES6 class.` +
-			` You give ${typeof arg}. You may add other types and send pull request.`
-	);
+  throw new TypeError(
+    `The toMock method support object and ES6 class.` +
+      ` You give ${typeof arg}. You may add other types and send pull request.`
+  );
 }
 
 function toMockedInstance(arg, overrides = {}) {
-	let mockedArg = toMock(arg);
+  let mockedArg = toMock(arg);
 
-	if (typeof mockedArg === 'function') {
-		let instance = Reflect.construct(mockedArg, []);
+  if (typeof mockedArg === 'function') {
+    let instance = Reflect.construct(mockedArg, []);
 
-		return Object.assign(instance, overrides);
-	}
+    return Object.assign(instance, overrides);
+  }
 
-	return Object.assign(mockedArg, overrides);
+  return Object.assign(mockedArg, overrides);
 }
 
 function mockClass(ClassConstructor) {
-	class Mock {}
+  class Mock {}
 
-	Reflect.setPrototypeOf(Mock.prototype, ClassConstructor.prototype);
-	Reflect.setPrototypeOf(Mock, ClassConstructor);
+  Reflect.setPrototypeOf(Mock.prototype, ClassConstructor.prototype);
+  Reflect.setPrototypeOf(Mock, ClassConstructor);
 
-	mockPrototypeChain(Mock.prototype);
-	mockStatic(ClassConstructor, Mock);
+  mockPrototypeChain(Mock.prototype);
+  mockStatic(ClassConstructor, Mock);
 
-	return Mock;
+  return Mock;
 }
 
 function mockObject(object) {
-	let newObject = Object.create(object);
-	mockPrototypeChain(newObject);
+  let newObject = Object.create(object);
+  mockPrototypeChain(newObject);
 
-	return newObject;
+  return newObject;
 }
 
 function mockStatic(ClassConstructor, Mock) {
-	while(ClassConstructor && Mock) {
-		mockOwnProperties(ClassConstructor, Mock);
+  while (ClassConstructor && Mock) {
+    mockOwnProperties(ClassConstructor, Mock);
 
-		ClassConstructor = Reflect.getPrototypeOf(ClassConstructor);
-		Mock = Reflect.getPrototypeOf(Mock);
-	}
+    ClassConstructor = Reflect.getPrototypeOf(ClassConstructor);
+    Mock = Reflect.getPrototypeOf(Mock);
+  }
 }
 
 function mockPrototypeChain(prototype) {
-	let originalPrototype = prototype;
+  let originalPrototype = prototype;
 
-	while (prototype) {
-		let clonePrototype = Object.create(prototype, {
-			[PROTOTYPE_CHAIN_MOCKED]: {
-				value: true,
-				enumerable: false
-			}
-		});
+  while (prototype) {
+    let clonePrototype = Object.create(prototype, {
+      [MOCKED_PROTOTYPE_CHAIN]: {
+        value: true,
+        enumerable: false
+      }
+    });
 
-		mockOwnProperties(prototype, clonePrototype);
+    mockOwnProperties(prototype, clonePrototype);
 
-		Reflect.setPrototypeOf(originalPrototype, clonePrototype);
+    Reflect.setPrototypeOf(originalPrototype, clonePrototype);
 
-		if (
-			originalPrototype[PROTOTYPE_CHAIN_MOCKED] &&
-			prototype[PROTOTYPE_CHAIN_MOCKED] &&
-			originalPrototype !== prototype
-		) {
-			return;
-		}
+    if (
+      originalPrototype[MOCKED_PROTOTYPE_CHAIN] &&
+      prototype[MOCKED_PROTOTYPE_CHAIN] &&
+      originalPrototype !== prototype
+    ) {
+      return;
+    }
 
-		originalPrototype = prototype;
-		prototype = Reflect.getPrototypeOf(prototype);
-	}
+    originalPrototype = prototype;
+    prototype = Reflect.getPrototypeOf(prototype);
+  }
 }
 
 function mockOwnProperties(original, mock) {
-	Object.entries(Object.getOwnPropertyDescriptors(original))
-		.forEach(([property, descriptor]) => {
-			try {
-				if (
-					(descriptor.get && typeof descriptor.get === 'function') ||
-					(descriptor.set && typeof descriptor.set === 'function')
-				) {
-					let _mockedValue = undefined;
+  Object.entries(Object.getOwnPropertyDescriptors(original)).forEach(
+    ([property, descriptor]) => {
+      try {
+        if (
+          (descriptor.get && typeof descriptor.get === 'function') ||
+          (descriptor.set && typeof descriptor.set === 'function')
+        ) {
+          let _mockedValue = undefined;
 
-					Object.defineProperty(mock, property, Object.assign({}, descriptor, {
-						get: () => _mockedValue,
-						set: (value) => { _mockedValue = value }
-					}));
-				}
+          Object.defineProperty(
+            mock,
+            property,
+            Object.assign({}, descriptor, {
+              get: () => _mockedValue,
+              set: value => {
+                _mockedValue = value;
+              }
+            })
+          );
+        }
 
-				if (original[property] && typeof original[property] === 'function') {
-					mock[property] = function mockMethod() {};
-				}
-			} catch(_) {}
-		});
+        if (original[property] && typeof original[property] === 'function') {
+          mock[property] = function mockMethod() {};
+        }
+      } catch (_) {} // eslint-disable-line no-empty
+    }
+  );
 }
 
 exports.default = toMock;
