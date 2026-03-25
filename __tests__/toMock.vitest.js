4f897651400f5b0b063142b327eb4ed172fea78b
Filip Satek <filip.satek@firma.seznam.cz>
2026-03-25 14:05:28 +0100

    test(tdd): toMock(class {}) breaks vitest afterAll script

---
+++ __tests__/toMock.vitest.js
@@ -0,0 +1,11 @@
+import { describe, it, afterAll } from 'vitest';
+import { toMock } from '../toMock';
+
+describe('toMock', () => {
+  // Defining `afterAll` leads to `TypeError: Cannot read properties of undefined (reading 'length')`
+  afterAll(() => {});
+
+  it('should not affect class prototype', () => {
+    toMock(class {});
+  });
+});
