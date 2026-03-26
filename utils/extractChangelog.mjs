6f0bcb547d07df808814c10dd235492b7cf9cf69
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2026-03-26 09:00:18 +0100

    chore: improve changelog extractor for more formats

--- utils/extractChangelog.mjs
+++ utils/extractChangelog.mjs
@@ -1,11 +1,12 @@
 import fs from 'node:fs';
 import path from 'node:path/posix';
 
-const VERSION_RE = /#+ [<>a-z]*\d+\.\d+\.\d+.+/gi;
+const ANCHOR_RE = /<a name="\d+\.\d+\.\d+"><\/a>\n/g;
+const SPLIT_RE = /(?:^|\n)(?=#{1,2} (?:\[|<small>)?\d+\.\d+\.\d+)/;
 const CHANGELOG = path.resolve(process.cwd(), 'CHANGELOG.md');
 
 const changelogContents = fs.readFileSync(CHANGELOG, 'utf-8');
-const versionChangelogs = changelogContents.split(VERSION_RE);
+const versionChangelogs = changelogContents.replace(ANCHOR_RE, '').split(SPLIT_RE);
 versionChangelogs.shift();
 
 fs.writeFileSync(
